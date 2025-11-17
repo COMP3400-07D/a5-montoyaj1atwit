018b4baa3ef0e8983a02f8c32efcc7facdcf64dc
Joey Montoya <jm5689220@gmail.com>
2025-11-17 16:18:00 -0500

    finished

--- partb.c
+++ partb.c
@@ -6,6 +6,41 @@
 #include <sys/wait.h>
 
 int main(int argc, char* argv[]) {
-    // TODO: Complete and document
-    return 0;
+    if (argc != 3) {
+        printf("USAGE: partb FILEIN FILEOUT\n");
+        return 1;
+    }
+
+    const char *input_filename = argv[1];
+    const char *output_filename = argv[2];
+
+    int fd = open(output_filename, O_WRONLY | O_CREAT | O_TRUNC, 0770);
+    if (fd < 0) {
+        perror("open");
+        return 2;
+    }
+
+    if (dup2(fd, STDOUT_FILENO) < 0) {
+        perror("dup2");
+        close(fd);
+        return 3;
+    }
+
+    close(fd);
+
+    // Print CSV header before exec'ing parta
+    printf("Category,Count\n");
+    fflush(stdout);
+
+    char *parta_args[] = {
+        "parta",
+        (char *)input_filename,
+        NULL
+    };
+
+    execv("./parta", parta_args);
+
+    // If we reach here, execv failed
+    perror("execv");
+    return 4;
 }
--- partc.c
+++ partc.c
@@ -6,7 +6,66 @@
 #include <sys/wait.h>
 
 int main(int argc, char* argv[]) {
-    // TODO: Complete and document
+    if (argc != 2) {
+        printf("USAGE: partc FILEIN\n");
+        return 1;
+    }
 
-    return 0;
+    const char *input_filename = argv[1];
+
+    int pipefd[2];
+    if (pipe(pipefd) < 0) {
+        perror("pipe");
+        return 2;
+    }
+
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        return 3;
+    }
+
+    if (pid > 0) {
+        // Parent: become parta, writing into the pipe
+        close(pipefd[0]);  // close read end
+
+        if (dup2(pipefd[1], STDOUT_FILENO) < 0) {
+            perror("dup2");
+            close(pipefd[1]);
+            return 4;
+        }
+        close(pipefd[1]);
+
+        char *parta_args[] = {
+            "parta",
+            (char *)input_filename,
+            NULL
+        };
+
+        execv("./parta", parta_args);
+        perror("execv parta");
+        return 5;
+    } else {
+        // Child: become sort, reading from the pipe
+        close(pipefd[1]);  // close write end
+
+        if (dup2(pipefd[0], STDIN_FILENO) < 0) {
+            perror("dup2");
+            close(pipefd[0]);
+            _exit(4);
+        }
+        close(pipefd[0]);
+
+        char *sort_args[] = {
+            "sort",
+            "-t,",
+            "-k2",
+            "-n",
+            NULL
+        };
+
+        execvp("sort", sort_args);
+        perror("execvp sort");
+        _exit(5);
+    }
 }
